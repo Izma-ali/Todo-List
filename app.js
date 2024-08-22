a036c493cb7293339eb7cd905a5b675b8c183cbb
Izma-ali <alimuhammadsameed@gmail.com>
2024-08-22 20:42:37 +0500

    added

---
+++ app.js
@@ -0,0 +1,10 @@
+var ulList = document.getElementById("list") 
+
+function addBtn(){
+    var input = document.getElementById("userInput").value
+    var list = document.createElement("li")
+console.log("");
+
+// list.innerHTML=input.value
+// input.value = ""
+}
