798b276ef60b933dae6a0f2c152383cf0fd2cf6f
Izma-ali <alimuhammadsameed@gmail.com>
2024-08-26 05:20:29 +0500

    task done

--- app.js
+++ app.js
@@ -1,10 +1,52 @@
-var ulList = document.getElementById("list") 
+var ulList = document.getElementById("list")
 
-function addBtn(){
-    var input = document.getElementById("userInput").value
-    var list = document.createElement("li")
-console.log("");
+function addBtn() {
+    var input = document.getElementById("userInput")
+    var li = document.createElement("li")
+    if (input.value == "") {
+        Swal.fire({
+            icon: "warning",
+            title: "Oops...",
+            text: "Please enter some task!",
+        });
+    }
+    else {
+        li.innerHTML = input.value
+        input.value = ""
+        ulList.appendChild(li)
+        li.setAttribute("class", "listItem")
 
-// list.innerHTML=input.value
-// input.value = ""
+        var btnsSpan = document.createElement("span")
+        btnsSpan.className = "btndiv"
+        li.appendChild(btnsSpan)
+
+        var editBtn = document.createElement("button")
+        editBtn.innerHTML = "<i class='fa-regular fa-pen-to-square'></i>";
+        editBtn.className = "dltBtnStyle";
+        editBtn.setAttribute("onclick", "edit(event)")
+        editBtn.setAttribute("type", "button")
+        btnsSpan.appendChild(editBtn)
+
+        var deleteBtn = document.createElement("button")
+        deleteBtn.innerHTML = "<i class='fa-regular fa-trash-can'></i>";
+        deleteBtn.className = "dltBtnStyle";
+        deleteBtn.setAttribute("onclick", "removelist(event)")
+        deleteBtn.setAttribute("type", "button")
+        btnsSpan.appendChild(deleteBtn)
+    }
+}
+
+function removelist(event){
+    event.target.closest("li").remove();
+}
+
+function edit(event){
+    var updated = prompt("Update task", event.target.closest("li").firstChild.nodeValue);
+    if (updated !== null) {
+      event.target.closest("li").firstChild.nodeValue = updated;
+    }
+}
+
+function dltAll(){
+    ulList.innerHTML = "";
 }
